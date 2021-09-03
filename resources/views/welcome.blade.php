209444b6b89e2b70a021f5fdb6fbbc38f87a2d49
Jimbriel <86084704+Jimbriel@users.noreply.github.com>
2021-09-03 17:00:00 -0500

    Implementacion Autenticacion
    Login y registro
    Se modificaron las rutas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,19 @@
 @extends("layout")
 @section("content")
-
+@auth
+{{auth()->user()->name}}
     <div class="container pt-3">
+
         <h4 class="text-danger mb-0"><strong>Bienvenidos al Bingo</strong></h4>
         <h1 class="text-primary"><strong>#yomequedoencasa</strong></h1>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
         <hr>
-
         @livewire("bingo")
         
     </div>
+@else
 
+@endauth
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/bingo', function () {
     return view('welcome');
-});
+})->middleware('auth');
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
